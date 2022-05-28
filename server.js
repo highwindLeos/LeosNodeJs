404690bd029001af538a0ff67282b4eebee7dcb8
Leos <highwind26@gmail.com>
2022-05-28 22:03:59 +0900

    HTML 불러오기

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
-const server = app.listen(8090, () => {
-    console.log('server Start~! 8090');
+app.listen(8090, () => {
+    console.log('Port 8090 Server Start~! ');
+});
+
+// URL 요청 : /
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/index.html'); // 파일전송
+});
+
+// URL 요청 : /write
+app.get('/write', (req, res)=>{
+    res.sendFile(__dirname + '/write.html');
 });
